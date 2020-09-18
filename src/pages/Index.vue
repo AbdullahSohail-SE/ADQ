e342c6bc70b3647a570cf0370dc5fe86ff1f5c0e
Abdullah Sohail <abdullahsohailofficial123@gmail.com>
2020-09-18 15:37:15 +0500

    Initial

---
+++ src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-btn color="primary" icon="facebook" label="Get Details" @click="getDetails" />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  methods:{
+    getDetails(){
+      facebookConnectPlugin.login(['public_profile','email'],function(success){
+        alert(JSON.stringify(success));
+      },function(failure){
+        alert('Failure!');
+      })
+    }
+  }
+}
+</script>
